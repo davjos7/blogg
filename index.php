afda8705427a6528f130e5c5a2c53a4bf15bafa4
davjos7 <josephdavilahorna@gmail.com>
2022-04-27 21:58:44 -0500

    plantilla worpress

--- index.php
+++ index.php
@@ -7,14 +7,20 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="icon" type="text/css" href="https://cdn-icons-png.flaticon.com/512/88/88290.png">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
-	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url');?>/estilo.css">
 </head>
 <body>
 <center>
+
+		
+
+
+
 	<header class="container-fluid"> 	
 		<div class="row">
 			<div class="col-lg-3">
 					<img src="https://cdn-icons-png.flaticon.com/512/88/88290.png" width="100px" 	class="tit">
+					<h1><?php bloginfo('name');?></h1>
 			</div>
 			<div class="col-lg-9">
 	        <ul class="menu">
@@ -29,7 +35,19 @@
 
 </div>
 	</header>
-	<hr>
+	<?php $articulos = new WP_Query([
+			'showposts'=> 3,
+		]);
+
+		while ($articulos->have_posts()) {
+			$articulos->the_post();
+
+			the_title();
+			echo '<hr>';
+			// code...
+		}?>
+
+		
    
  <section class="container">
    		
