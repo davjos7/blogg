37499374026d994cdddcf6dcaa702bbdb0e859c8
davjos7 <josephdavilahorna@gmail.com>
2022-04-28 21:50:10 -0500

    edite mas

--- index.php
+++ index.php
@@ -40,56 +40,34 @@
 		]);
 
 		while ($articulos->have_posts()) {
-			$articulos->the_post();
+			$articulos->the_post();?>
 
-			the_title();
-			echo '<hr>';
-			// code...
-		}?>
+
+
+				<?php the_post_thumbnail("medium	"); ?>
+
+
+
+
+		
+   		<h2><?php the_title(); ?></h2>
+
+
+
+         <?php the_excerpt(); ?>	
+
+
+
+
+		
+		<?php } ?>
+
+		
+		
 
 		
    
- <section class="container">
-   		
-   	<div>
-   		<img src="https://as01.epimg.net/meristation/imagenes/2021/03/22/noticias/1616401362_122521_1616401499_noticia_normal.jpg" width="200px" class="imagen"><br>
-
-   		<h2>Final De Anime</h2>
-
-    <p>El último capítulo de “Shingeki no Kyojin” se estrenará el domingo 3 de marzo de 2022 en Japón a través de NHK y en América Latina, de manera online, a través la plataforma Crunchyroll.</p>
-
-
-   	</div>
-   	<hr>
-   	<div>
-
-   		<img src="https://depor.com/resizer/IDDHG73kzYo7pFWtlNBXr-a2yWc=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/O4W2UN5F7RB7FHC7SR67TWVEWY.jpg"
-   		width="200px"><br>
-
-   		<h2>Final De Demon Slayer</h2>
-   		
-     
-    <p>El rango superior se reúne todos juntos una vez que se cierra el 'Arco del Distrito de Entretenimiento' y uno de los suyos se pierde. Akaza aparece aún dolido por su batalla con Rengoku, y no está solo. Otros como Gyokko aparecen en esta reunión, pero todos los ojos están puestos en Kokushibo.</p>
-
-
-   	</div>
-   	<hr>
-   
-   	<div>
-   		<img src="https://areajugones.sport.es/wp-content/uploads/2021/03/imagen-2021-03-18-083639-1080x609.jpg" width="200px"><br>
-
-   		<h2>Final De Black clover</h2>
-   		
-     
-    <p>No hace ni 24 horas que se producían las filtraciones en torno a que el manga de "Black Clover" se disponía a estar un breve período de tiempo completamente ausente. Lo cierto es que en ese entonces no se sabía ni el motivo ni cuánto tiempo se iban a quedar los lectores sin Asta y compañía, una serie de dudas que ahora se han visto disipadas. Todo tiene su final, y parece ser que después de 7 años de publicación ininterrumpida, Yuki Tabata se dispone a darle sus últimas pinceladas a Asta y compañía</p>
-
-
-   	</div>
-   	<hr>
-   
-     
- </section>
-
+		
  <footer>
  	<ul class="pie">
  		<li><a href="#"><i class="fa fa-facebook"></i></a></li>
