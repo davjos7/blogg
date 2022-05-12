e60ffa20e6002ef0fdb35f69c2a0a475df36f0be
davjos7 <josephdavilahorna@gmail.com>
2022-05-11 21:56:48 -0500

    puse tipo de wp

--- single.php
+++ single.php
@@ -2,12 +2,19 @@
   <?php get_header(); ?>
   	<section class="container">
   		<div>
-  			<img src="<?php the_post_thumbnail_url(); ?>"><br>
+  			
 
   			<h2><?php the_title(); ?></h2>
 
+  			<h4 style="color: white;"><?php the_field('resumen'); ?></h4>
+
+  			<img src="<?php the_post_thumbnail_url(); ?>"><br>
+
 
   			 <div class="text-center" style="color: white; "><?php the_content(); ?></div>
+
+  			 <h4 style="color: white;">Fuente: <?php the_field('fuente'); ?></h4>
+
 
 			  	<ul>
 
